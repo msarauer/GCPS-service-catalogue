34a52f16ac88db76f99d63bebce455926f6bdae9
Mitchel Sarauer <Mitchel.Sarauer@tpsgc-pwgsc.gc.ca>
2022-07-04 15:38:45 -0600

    Added prototype ended message

--- src/components/BetaBanner.jsx
+++ src/components/BetaBanner.jsx
@@ -6,19 +6,27 @@ const BetaBanner = ({ lang }) => {
       <div className="container">
         {lang === "en" ? (
           <span style={{ fontSize: "15px" }}>
-            This is the{" "}
-            <strong className="underline">
-              <u>Alpha</u>
-            </strong>{" "}
-            version of the website.
+            <strong>
+              Development on this prototype has stopped and the content found on
+              this page is no longer being maintained as of June 29, 2022. If
+              you require more information regarding topics found on this page,
+              please send an email to{" "}
+              <a href="mailto: Joseph.Moubayed@tpsgc-pwgsc.gc.ca">
+                Joseph.Moubayed@tpsgc-pwgsc.gc.ca
+              </a>
+            </strong>
           </span>
         ) : (
           <span style={{ fontSize: "15px" }}>
-            Il s'agit de la version{" "}
-            <strong className="underline">
-              <u>Alpha</u>
-            </strong>{" "}
-            du site web.
+            <strong>
+              Le développement de ce prototype a été interrompu, et le contenu
+              qui se trouve sur cette page n’est plus entretenu depuis le 29
+              juin 2022. Pour toute demande d’information concernant les sujets
+              figurant sur cette page, veuillez communiquer avec{" "}
+              <a href="mailto: Joseph.Moubayed@tpsgc-pwgsc.gc.ca">
+                Joseph.Moubayed@tpsgc-pwgsc.gc.ca
+              </a>
+            </strong>
           </span>
         )}
       </div>
